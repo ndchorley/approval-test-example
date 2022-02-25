6f6debbbe6fad2438b8315f4ec70684ae1eabb92
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-02-25 20:08:59 +0000

    Refactor to pull out summaryLine extension function

--- src/main/kotlin/com/example/test_reporter.kt
+++ src/main/kotlin/com/example/test_reporter.kt
@@ -1,14 +1,18 @@
 package com.example
 
 fun report(tests: List<Test>): String {
-    val (passes, fails) = tests.partition { test -> test.result == Result.PASSED }
-
     return tests
-        .reportLines()
+        .resultLines()
         .plus("")
-        .plus("${tests.size} tests run, ${passes.size} passed, ${fails.size} failed")
+        .plus(tests.summaryLine())
         .joinToString(System.lineSeparator())
 }
 
-private fun List<Test>.reportLines(): List<String> =
+private fun List<Test>.resultLines(): List<String> =
     this.map { test -> "${test.result}: ${test.description}" }
+
+private fun List<Test>.summaryLine(): String {
+    val (passes, fails) = this.partition { test -> test.result == Result.PASSED }
+
+    return "$size tests run, ${passes.size} passed, ${fails.size} failed"
+}
